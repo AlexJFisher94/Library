093babcf4139cb0088e94e961ad3ed0a7bec4767
AlexJFisher94 <alexjfisher1994@gmail.com>
2023-01-02 22:32:44 -0500

    Added variables and functions to create new book object

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const modal = document.querySelector(".modal");
 const addBook = document.querySelector(".add-btn");
 const closeButton = document.querySelector(".close-button");
+const submit = document.querySelector("#submit");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -8,3 +13,30 @@ function toggleModal() {
 
 addBook.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
+
+let myLibrary = [];
+let thisBook;
+
+function Book () {
+    this.title = title.value;
+    this.author = author.value;
+    this.pages = pages.value;
+    this.value = read.value;
+}
+
+submit.addEventListener("click", function(e) {
+    e.preventDefault();
+    thisBook = new Book();
+    console.log(thisBook);
+    addBookToLibrary(thisBook);
+    toggleModal();
+})
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
+
+
+
+
