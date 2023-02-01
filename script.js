bce2af6101d2e4cb4c467e3f18732d9137d61f9c
AlexJFisher94 <alexjfisher1994@gmail.com>
2023-01-31 22:21:48 -0500

    Rewrote entire application to more easily implement 'delete' and 'change read status' buttons

--- script.js
+++ script.js
@@ -1,42 +1,105 @@
 const modal = document.querySelector(".modal");
+const form = document.querySelector("form");
 const addBook = document.querySelector(".add-btn");
-const closeButton = document.querySelector(".close-button");
+const closeModal = document.querySelector(".close-button");
 const submit = document.querySelector("#submit");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const read = document.getElementById("read");
+const books = document.querySelector(".books");
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("read");
 
+// declare global variables
+let myLibrary = [];
+let removeButton;
+
+// add event listener for "Add Book" button
+addBook.addEventListener("click", toggleModal);
+closeModal.addEventListener("click", toggleModal);
+
+// toggles modal window
 function toggleModal() {
     modal.classList.toggle("show-modal");
 }
 
-addBook.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-
-let myLibrary = [];
-let thisBook;
-
-function Book () {
-    this.title = title.value;
-    this.author = author.value;
-    this.pages = pages.value;
-    this.value = read.value;
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-submit.addEventListener("click", function(e) {
-    e.preventDefault();
-    thisBook = new Book();
-    console.log(thisBook);
-    addBookToLibrary(thisBook);
+submit.addEventListener('click', function() {
+    event.preventDefault();
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let read = bookRead.value;
+    let book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
     toggleModal();
 })
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-    console.log(myLibrary);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    renderBooks();
 }
 
+function createBookItem(book) {
+    let newBook = document.createElement("div");
+    let title = document.createElement("h1");
+    let author = document.createElement("h1");
+    let pages = document.createElement("h1");
+    let read = document.createElement("h1");
+    let readButton = createReadBtn(book);
+    let removeBookBtn = createDeleteBtn(book);
+    title.innerHTML = `Title: ${book.title}`;
+    author.innerHTML = `Author: ${book.author}`;
+    pages.innerHTML = `Pages: ${book.pages}`;
+    read.innerHTML = `Read? ${book.read}`;
+    newBook.classList.add("cardStyle");
+    newBook.append(title, author, pages, read, readButton, removeBookBtn);
+    books.appendChild(newBook);
+    form.reset();
+}
 
+function createDeleteBtn(book) {
+    removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove Book";
+    let index = myLibrary.indexOf(book);
+    removeButton.setAttribute("id", index);
+    removeButton.addEventListener('click', deleteBook);
+    return removeButton;
+}
 
+function createReadBtn(book) {
+    readButton = document.createElement("button");
+    readButton.innerHTML = "Change Read Status";
+    let index = myLibrary.indexOf(book);
+    readButton.setAttribute("id", index);
+    readButton.addEventListener('click', changeReadStatus);
+    return readButton;
+}
 
+function deleteBook(e) {
+    let index = e.target.getAttribute("id");
+    myLibrary.splice(index, 1);
+    renderBooks();
+}
+
+function changeReadStatus(e) {
+    let index = e.target.getAttribute("id");
+    if (myLibrary[index].read === "Yes") {
+        myLibrary[index].read = "No";
+    } else {
+        myLibrary[index].read = "Yes";
+    }
+    renderBooks();
+}
+
+function renderBooks() {
+    books.textContent = "";
+    myLibrary.map((book) => {
+        createBookItem(book);
+    });
+}
